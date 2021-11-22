68716a023ceeea70a2bec898515624a9152c6539
Patryk Studzinski <studzin-sky>
2021-11-22 22:06:04 +0100

    Pushed data with useState to LoreData

--- src/app/components/Content/Content.js
+++ src/app/components/Content/Content.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./Content.css";
 import {SearchForm} from '../SearchForm/SearchForm';
@@ -6,15 +6,20 @@ import {LoreData} from '../LoreData';
 
 
 export const Content = (props) => {
+
+  const [pulledType, setPulledType] = useState('');
   
   const returnedType = type => {
-    console.log(type);
+    setPulledType(type);
   }
+  
+  const pulledTypeVal = pulledType;
+
 
   return (
   <div>
     <SearchForm onReturnedType = {returnedType} />
-    <LoreData items={props.items} />
+    <LoreData items={props.items} type={pulledTypeVal}/>
   </div>
   );
 };
